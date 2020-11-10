6d50eb65fb03a81a22560833f245cea45e326e4a
rightfootmessi <aroni.g1011@gmail.com>
2020-11-10 16:18:17 -0500

    Quests command is fully functional now

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const https = require('https');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
 const cmdPrefix = 'd!';
 
@@ -19,23 +18,18 @@ client.on('ready', () => {
 			body.push(chunk);
 		}).on('end', () => {
 			body = Buffer.concat(body).toString();
-			var htmlDoc = new JSDOM(body).window.document;
-			var questsTableHTML = htmlDoc.getElementById('tabber-9effcf7958cd8e73b6b03de0c8c97743').getElementsByClassName('tabbertab')[0].getElementsByTagName('table')[0];
-			var entries = questsTableHTML.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-			console.log("Found " + entries.length + " table entries");
-			console.log(entries);
+			const $ = cheerio.load(body);
+			var questsTableHTML = $('#tabber-9effcf7958cd8e73b6b03de0c8c97743');//.getElementsByClassName('tabbertab')[0].getElementsByTagName('table')[0];
+			var entries = questsTableHTML.find('tbody').first();//.children('tr').each((i) => console.log("Entry " + i + ": " + $(this).html()));
 			var numLoaded = 0;
-			for (const entry of entries) {
-				var boxes = entry.getElementsByTagName('td');
-				if (boxes.length > 0) {
-					console.log(boxes);
-
-					var qName = boxes.item(0).innerText;
-					var qDragon = boxes.item(2).innerText;
-					console.log("Added quest to table - Name: " + qName + "; Dragon: " + qDragon);
+			entries.children('tr').each((i, elem) => {
+				if (i > 0) {
+					var qName = $(elem).children('td').eq(0).text().trim().toLowerCase();
+					var qDragon = $(elem).children('td').eq(2).text().trim();
+					questTable[qName] = qDragon;
 					numLoaded++;
 				}
-			}
+			});
 			console.log(numLoaded + " quests loaded!");
 			questsLoaded = true;
 		}).on('error', (e) => {
@@ -50,15 +44,21 @@ client.on('message', message => {
 	const args = message.content.slice(cmdPrefix.length).trim().split(" ");
 	const cmd = args.shift().toLowerCase();
 	if (cmd === 'quest') {
+		if (!questsLoaded) {
+			message.channel.send("Quests have not been loaded yet!");
+			return;
+		}
 		var questname = args[0];
 		for (i = 1; i < args.length; i++) {
 			questname += " " + args[i];
 		}
-		message.channel.send("You are looking for the dragon whose quest is " + questname);
-		if (!questsLoaded) message.channel.send("Quests have not been loaded yet!");
+		if (questTable[questname.toLowerCase()]) message.channel.send("The dragon for " + questname + " is " + questTable[questname.toLowerCase()]);
+		else message.channel.send("Selfhost: " + questname + " is not a recognized quest (did you type it correctly?)");
 	} else {
 		message.channel.send("Received unhandled command " + cmd + " with arguments " + args);
 	}
 });
- 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+// Comment/uncomment before committing to git
+//client.login(process.env.BOT_TOKEN);
+client.login('Nzc1MzgyODQyNjcwMDU1NDI1.X6lhiw.T0cDEvKNISj5VyBuWNpGWwn479s');
