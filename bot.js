a439b43b742ed6d364068887f2d6b98c8b0a5c18
rightfootmessi <aroni.g1011@gmail.com>
2020-11-10 23:47:23 -0500

    Added elements command

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const cheerio = require('cheerio');
 
 const cmdPrefix = 'd!';
 
-const dragonList = [];
+const primaries = ["Plant Dragon", "Fire Dragon", "Earth Dragon", "Cold Dragon", "Lightning Dragon", "Water Dragon", "Air Dragon", "Metal Dragon", "Light Dragon", "Dark Dragon"];
+const dragonList = ["Plant Dragon", "Fire Dragon", "Earth Dragon", "Cold Dragon", "Lightning Dragon", "Water Dragon", "Air Dragon", "Metal Dragon", "Light Dragon", "Dark Dragon", "Monolith Dragon", "Snowflake Dragon"];
 const questTable = {};
 var questsLoaded = false;
 
@@ -13,15 +14,15 @@ client.on('ready', () => {
 	console.log('I am ready!');
 	
 	https.get('https://dragonvale.fandom.com/wiki/Quests', (res) => {
-		console.log("Received " + res.statusCode + " status code");
+		console.log("Received " + res.statusCode + " status code for quest request");
 		var body = [];
 		res.on('data', (chunk) => {
 			body.push(chunk);
 		}).on('end', () => {
 			body = Buffer.concat(body).toString();
 			const $ = cheerio.load(body);
-			var questsTableHTML = $('#tabber-9effcf7958cd8e73b6b03de0c8c97743');//.getElementsByClassName('tabbertab')[0].getElementsByTagName('table')[0];
-			var entries = questsTableHTML.find('tbody').first();//.children('tr').each((i) => console.log("Entry " + i + ": " + $(this).html()));
+			var questsTableHTML = $('#tabber-9effcf7958cd8e73b6b03de0c8c97743');
+			var entries = questsTableHTML.find('tbody').first();
 			var numLoaded = 0;
 			entries.children('tr').each((i, elem) => {
 				if (i > 0) {
@@ -51,10 +52,7 @@ client.on('message', message => {
 			message.channel.send("Quests have not been loaded yet!");
 			return;
 		}
-		var questname = args[0];
-		for (i = 1; i < args.length; i++) {
-			questname += " " + args[i];
-		}
+		var questname = prettyString(args, " ");
 		if (!questname) {
 			message.channel.send("You must give me a quest name to look for!");
 			return;
@@ -63,37 +61,84 @@ client.on('message', message => {
 		if (dragon) message.channel.send("Use a(n) **" + dragon + "** to complete the quest \"" + questname + "\"");
 		else message.channel.send("\"" + questname + "\" is not a recognized quest name (did you type it correctly?)");
 	} else if (cmd === 'breed') {
-		var dragon = args[0].toLowerCase();
-		dragon = dragon.charAt(0).toUpperCase() + dragon.substring(1);
-		for (i = 1; i < args.length; i++) {
-			var str = args[i].toLowerCase();
-			str = str.charAt(0).toUpperCase() + str.substring(1);
-			dragon += " " + str;
+		var dragon = prettyString(args, " ");
+		if (!dragon) {
+			message.channel.send("You must specify a dragon!");
+			return;
 		}
-		if (args[args.length - 1].toLowerCase() != 'dragon') {
+		if (dragon.indexOf("Dragon") == -1) {
 			dragon += " Dragon";
 		}
 		if (!dragonList.includes(dragon)) {
 			message.channel.send("Unrecognized dragon name \"" + dragon + "\" (did you spell it correctly?)");
 			return;
 		}
+		if (isPrimary(dragon)) {
+			message.channel.send(dragon + " is a primary dragon, just breed two of them together to get more...");
+			return;
+		}
 		var dragon_ = dragon.replace(/ /g, "_");
 		https.get('https://dragonvale.fandom.com/wiki/' + dragon_, (res) => {
-			console.log("Received " + res.statusCode + " status code");
+			console.log("Received " + res.statusCode + " status code for breeding request");
 			var body = [];
 			res.on('data', (chunk) => {
 				body.push(chunk);
 			}).on('end', () => {
 				body = Buffer.concat(body).toString();
 				const $ = cheerio.load(body);
-				message.channel.send($("#Breeding").parent().next().text().trim());	
+				response = $("#Breeding").parent().next().text().trim();
+				if ($("td[style='border-top-style:hidden;border-left-style:hidden;']").last().text().trim() === "EXPIRED") {
+					response += " *Note: This dragon is not available right now (per the wiki)!*";
+				}
+				message.channel.send(response);	
+			});
+		});
+	} else if (cmd === 'elements') {
+		var dragon = prettyString(args, " ");
+		if (!dragon) {
+			message.channel.send("You must specify a dragon!");
+			return;
+		}
+		if (dragon.indexOf("Dragon") == -1) {
+			dragon += " Dragon";
+		}
+		if (!dragonList.includes(dragon)) {
+			message.channel.send("Unrecognized dragon name \"" + dragon + "\" (did you spell it correctly?)");
+			return;
+		}
+		if (isPrimary(dragon)) {
+			message.channel.send(dragon + " is a primary dragon, its only element is in its name...");
+			return;
+		}
+		var dragon_ = dragon.replace(/ /g, "_");
+		https.get('https://dragonvale.fandom.com/wiki/' + dragon_, (res) => {
+			console.log("Received " + res.statusCode + " status code for elements request");
+			var body = [];
+			res.on('data', (chunk) => {
+				body.push(chunk);
+			}).on('end', () => {
+				body = Buffer.concat(body).toString();
+				const $ = cheerio.load(body);
+				var elems = [];
+				$(".dragonbox").first().find('tr').eq(17).children('td').first().children().each((i, elem) => {
+					var imgName = $(elem).children().first().attr('data-image-name');
+					if (!imgName.includes("Iconb")) {
+						elems.push(imgName.split(" ")[1].replace(".png", ""));
+					}
+				});
+				if (elems.length == 10) {
+					message.channel.send(dragon + " adds all 10 elements when breeding (often called a *pseudo*).");
+				} else {
+					message.channel.send(dragon + " adds the " + prettyString(elems, ", ") + " elements when breeding.");
+				}
 			});
 		});
 	} else if (cmd === 'help') {
 		const helpMsg = "Command list:(prefix all commands with `" + cmdPrefix + "`)\n"
 				+ "`quest <quest name>` - get the correct dragon to send on a quest\n"
-				+ "`help` - view this message\n"
-				+ "`breed <dragon name>` - find out how to breed a dragon";
+				+ "`breed <dragon name>` - find out how to breed a dragon\n"
+				+ "`elements <dragon name>` - get the breeding elements (aka hidden elements) of a dragon\n"
+				+ "`help` - view this message";
 		message.channel.send(helpMsg);
 	} else {
 		message.channel.send("Unknown command. Type `" + cmdPrefix + "help` for a list of commands");
@@ -101,5 +146,22 @@ client.on('message', message => {
 });
 
 // Comment/uncomment appropriately before committing to git
-client.login(process.env.BOT_TOKEN);
-//client.login('Nzc1MzgyODQyNjcwMDU1NDI1.X6lhiw.T0cDEvKNISj5VyBuWNpGWwn479s');
+//client.login(process.env.BOT_TOKEN);
+client.login('Nzc1MzgyODQyNjcwMDU1NDI1.X6lhiw.T0cDEvKNISj5VyBuWNpGWwn479s');
+
+prettyString = function(words, separator) {
+	if (words.length == 0) return false;
+	var result = words[0].toLowerCase();
+	result = result.charAt(0).toUpperCase() + result.substring(1);
+	for (i = 1; i < words.length; i++) {
+		var str = words[i].toLowerCase();
+		str = str.charAt(0).toUpperCase() + str.substring(1);
+		result += separator + str;
+	}
+	return result;
+}
+
+isPrimary = function(dName) {
+	dName = dName.replace(" Rift", "");
+	return primaries.includes(dName);
+}
