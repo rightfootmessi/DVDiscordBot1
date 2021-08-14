e983ae968bb49447ee3bf416ebbe523812c538c6
rightfootmessi <aroni.g1011@gmail.com>
2021-08-14 14:48:21 -0400

    fix monos/snowflakes with d!dvbox

--- bot.js
+++ bot.js
@@ -105,12 +105,12 @@ client.on('message', message => {
 		if (message.channel.type != 'dm' && message.guild.name == 'DragonVale' && (message.channel.name != 'bot-commands' && message.channel.name != 'oracle-test')) return;
 	}
 
-    if (args.includes("monolith") && cmd != 'result' && cmd != 'fakeouts') {
+    if (args.includes("monolith") && !(cmd === 'result' || cmd === 'fakeouts') && !(cmd === 'sandbox' || cmd === 'dvbox')) {
         if (isNaN(args[args.indexOf("monolith") + 1])) {
             message.channel.send("Your query contains a monolith dragon, but you did not specify which. Please re-enter the query using `Monolith #`, where `#` is replaced by the number of the monolith dragon.");
             return;
         }
-    } else if (args.includes("snowflake") && cmd != 'result' && cmd != 'fakeouts') {
+    } else if (args.includes("snowflake") && !(cmd === 'result' || cmd === 'fakeouts') && !(cmd === 'sandbox' || cmd === 'dvbox')) {
         if (isNaN(args[args.indexOf("snowflake") + 1])) {
             message.channel.send("Your query contains a snowflake dragon, but you did not specify which. Please re-enter the query using `Snowflake #`, where `#` is replaced by the number of the snowflake dragon.");
             return;
